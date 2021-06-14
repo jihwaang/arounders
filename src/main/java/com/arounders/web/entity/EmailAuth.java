63d8413ce93a4a269a9865a3f9f49834fb673dc6
jihwang <jihwaang@naver.com>
2021-06-14 12:38:09 +0900

    commit for me

--- src/main/java/com/arounders/web/entity/EmailAuth.java
+++ src/main/java/com/arounders/web/entity/EmailAuth.java
@@ -16,5 +16,6 @@ public class EmailAuth {
     private boolean isConfirmed;
     private LocalDateTime confirmedAt;
     private LocalDateTime createdAt;
+    private String confirmKey;
 
 }
--- src/main/java/com/arounders/web/serviceImpl/MailServiceImpl.java
+++ src/main/java/com/arounders/web/serviceImpl/MailServiceImpl.java
@@ -20,21 +20,31 @@ public class MailServiceImpl implements MailService {
     public void sendMail(RequestMail mail) {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper messageHelper = new MimeMessageHelper(message, "utf-8");
-        String htmlMsg = "<form action=\"127.0.0.1:8080/auth\" method=\"post\" style=\"width: 550px;\n" +
+        String url = "localhost:8080/index.html";
+        String onClick = "onclick=\"alert('회원가입을 마무리해주세요 :)')\"";
+        String htmlMsg = "<form style=\"width: 550px;\n" +
                 "      height: 200px;\n" +
                 "      display: flex;\n" +
                 "      flex-direction: column;\n" +
                 "      place-items: center;\">\n" +
                 "    <h1>Arounder에 오신것을 환영합니다.</h1>\n" +
                 "    <p>회원가입 인증을 위해 아래 인증버튼을 눌러주세요.</p>\n" +
-                "    <input type=\"submit\" id=\"auth\" value=\"인증하기\" onclick=\"alert('회원가입을 마무리해주세요 :)')\" style=\"width: 200px;\n" +
+                "    <div style=\"width: 200px;\n" +
                 "        height: 50px;\n" +
                 "        background-color: skyblue;\n" +
                 "        border: none;\n" +
                 "        color: #fff;\n" +
                 "        font-size: 1rem;\n" +
-                "        cursor: pointer\"/>\n" +
-                "</form>";
+                "        cursor: pointer;\n" +
+                "        display: flex;\n" +
+                "        align-items: center;\n" +
+                "        justify-content: center;\">\n" +
+                "        <a href=\""+url+"\" style=\"color: #fff; text-decoration: none; font-weight: bold;\">인증하기</a>\n" +
+                "    </div>\n" +
+                "\n" +
+                "\n" +
+                "</form>"
+                ;
         try {
             messageHelper.setText(htmlMsg, true);
             messageHelper.setTo("jihwaang@naver.com");
